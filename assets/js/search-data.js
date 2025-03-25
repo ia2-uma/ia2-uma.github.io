d1fea4200a41dd1bb738d516947e09123dabbc9e
ia2-uma <eguzman@uma.es>
2025-03-25 10:25:43 +0000

    Deploying to gh-pages from @ ia2-uma/ia2-uma.github.io@f084bb83dfa55e696b1d9e1cec7b29912e716585 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,51 +440,21 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-atreides",
+          title: 'ATREIDES',
+          description: "Agent-based simulation for predictive policing through data-driven and multi-tiered models",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/1_atreides/";
+            },},{id: "projects-aisen",
+          title: 'AISEN',
+          description: "Development and validation of an AI-based interactive environment for the assessment and optimization of executive functions and reading processes in developmental disorders",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/2_aisen/";
+            },},{id: "projects-federja",
+          title: 'FEDERJA',
+          description: "Agent-Based Crime Prediction Models and Data Science",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/3_federja/";
             },},{
         id: 'social-email',
         title: 'email',
